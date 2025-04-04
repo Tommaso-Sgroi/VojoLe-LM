08e391fc5812df3d6c4146909fa279cb94486b19
Tommaso-Sgroi <sgroi_tom@yahoo.com>
2025-04-04 22:10:55 +0200

    Enhance database schema and job server functionality with status management and scheduling

--- db/init.sql
+++ db/init.sql
@@ -1,12 +1,15 @@
 CREATE DATABASE IF NOT EXISTS sentence_db;
 USE sentence_db;
+# SET GLOBAL event_scheduler = ON;
 
 
 CREATE TABLE IF NOT EXISTS ItaSentence (
-    sentence_id CHAR(255) PRIMARY KEY,
+    sentence_id CHAR(47) PRIMARY KEY,
     sentence_text MEDIUMTEXT,
-    status int(4) DEFAULT -1
+    status int(4) DEFAULT -1,
+    time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 CREATE TABLE IF NOT EXISTS SorSentence (
     sentence_id CHAR(47) PRIMARY KEY,
     sentence_text MEDIUMTEXT
