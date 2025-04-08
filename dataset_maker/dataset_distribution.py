73a46246dfb4dacae1d70f878dd74558de6047a0
Tommaso-Sgroi <sgroi_tom@yahoo.com>
2025-04-08 18:48:46 +0200

    Add distribution frequency functions Add distribution frequency on test set

--- dataset_maker/dataset_distribution.py
+++ dataset_maker/dataset_distribution.py
@@ -3,38 +3,18 @@ the objective is about rebuild a dataset with the most uniform token distributio
 import string
 
 import nltk
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 from nltk import *
 from tqdm import tqdm
 from string import punctuation
 import re
-from dataset_maker.download_dataset import get_dataset_stream
+
+from tqdm.contrib.concurrent import process_map
+
+from dataset_maker.download_dataset import get_dataset_stream, TRAIN_SPLIT
 from dataset_maker.dataset import load_dataset
 
 italian_stopwords = nltk.corpus.stopwords.words('italian')
-
-
-# the following functions are taken from https://github.com/nikhiljsk/preprocess_nlp/blob/master/preprocess/preprocess_nlp.py
-def remove_tags(text):
-    """
-    Helper function for preprocess_nlp which is used to remove HTML Tags, Accented chars, Non-Ascii Characters, Emails and URLs
-
-    :param text: A string to be cleaned
-
-    <Returns the cleaned text>
-    """
-    # Remove HTML tags
-    soup = BeautifulSoup(text, "html.parser")
-    [s.extract() for s in soup(['iframe', 'script'])]
-    stripped_text = soup.get_text()
-    stripped_text = re.sub(r'[\r|\n|\r\n]+', '\n', stripped_text)
-
-    text = unicodedata.normalize('NFKD', stripped_text).encode('ascii', 'ignore').decode('utf-8',
-                                                                                         'ignore')  # Remove Accented characters
-    text = re.sub(r'[^\x00-\x7F]+', '', text)  # Remove Non-Ascii characters
-    text = re.sub("[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", '', text)  # Remove Emails
-    text = re.sub(r"http\S+", "", text)  # Remove URLs
-    return text
 
 def clean_text(text):
     text = text.lower()  # Lowercase
@@ -44,28 +24,49 @@ def clean_text(text):
     text = ' '.join([word for word in text.split() if word not in italian_stopwords])
     return text
 
-def stemming_distribution(dataset: list[str]):
-    distribution = dict()
+def stemming_distribution(dataset: list[str], use_tqdm=False) -> FreqDist:
+    frequency_distribution = FreqDist()
+
     # tokenized_dataset = tokenizer.tokenize_sents(dataset)
     stemmer = nltk.stem.SnowballStemmer(language='italian')
-    for id_sentence in tqdm(dataset):
-        sentence = id_sentence['text']
+    for sentence in tqdm(dataset) if use_tqdm else dataset:
+        # sentence = id_sentence
         sentence = clean_text(sentence)
         word_tokens = word_tokenize(sentence, language='italian')
-        for token in word_tokens:
-            pass
+        for i in range(len(word_tokens)):
+            token_stem = stemmer.stem(word_tokens[i])
+            word_tokens[i] = token_stem
+            # increment the found token of 1
+        frequency_distribution.update(word_tokens)
+    return frequency_distribution
 
-
-    return distribution
+# def lemmas_distribution(dataset: list[str], use_tqdm=False):
+#     distribution = dict()
+#     for sentence in tqdm(dataset) if use_tqdm else dataset:
+#         # sentence = id_sentence
+#         sentence = clean_text(sentence)
+#         tokenized_text = word_tokenize(sentence, language='italian')
+#         tokens_tag = pos_tag(tokenized_text, lang='ita')
+#         pass
+#         for i in range(len(word_tokens)):
+#             token_stem = stemmer.stem(word_tokens[i])
+#             word_tokens[i] = token_stem
+#
+#             distribution[token_stem] = 1 + distribution.get(token_stem, 0)
+#             # increment the found token of 1
+#
+#     return distribution
 
 
 if __name__ == '__main__':
-    import argparse
+    import argparse, json
     parser = argparse.ArgumentParser(
         prog='db-server',
         )
     parser.add_argument('--setup', action='store_true', help='Run nltk.download(\'all\' and exit.')
     parser.add_argument('--delete_data', action='store_true', help='Remove all NLTK data previously downloaded and exit.')
+    parser.add_argument('--skip_train_set', action='store_true', default=True, help='Skip the train set for the words frequency count.')
+    parser.add_argument('--parallel', '-p', nargs='?', type=int, help='Calculate distributions in parallel.', default=0)
     args = parser.parse_args()
 
     if args.setup:
@@ -81,12 +82,52 @@ if __name__ == '__main__':
                 shutil.rmtree(p)
         exit(0)
 
+    print('loading dataset\n')
     d = load_dataset()
-    dataset = d[0] + d[1]
-    del d
+    dataset = d[1] + d[0]
+    dataset = [e['text'] for e in dataset]
 
-    distribution = stemming_distribution(dataset)
-    pass
+    # del d
+    print('processing')
+    frequency_dist = FreqDist()
+    if args.parallel >= 1:
+        # for some reason is slower than single core
+        m_results = process_map(stemming_distribution, dataset, max_workers=args.parallel, miniters=1000)
+
+        # merge results
+        print('Merging test results')
+
+        for d in tqdm(m_results):
+            frequency_dist.update(d)
+    else:
+        frequency_dist = stemming_distribution(dataset, True)
+
+        if not args.skip_train_set:
+            stream = get_dataset_stream(TRAIN_SPLIT)
+
+            dataset.clear()
+            chunk_size = 50_000
+            i = 1
+            for entry in tqdm(stream, desc='Preparing chunk...', miniters=1):
+                dataset.append(entry['text'])
+                if not (i % chunk_size): # chunk size reached
+                    f = stemming_distribution(dataset, True)
+                    frequency_dist.update(f)
+                    dataset.clear()
+                i += 1
+            else:
+                # if for loop ends without reaching the chunk size
+                f = stemming_distribution(dataset, True)
+                frequency_dist.update(f)
+                dataset.clear()
+
+
+    with open('./data/fineweb2-test-distribution.json', 'w', encoding='utf-8') as f:
+        json.dump({k:v for k, v in frequency_dist.items()}, f) # convert in raw dict and store results
+
+    # print top 10 tokens
+    print(frequency_dist.most_common(10))
+
 
 
 
