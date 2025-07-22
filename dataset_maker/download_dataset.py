da1f9cbeacf9962618865cce8e683dfea96a1d08
Tommaso-Sgroi <sgroi_tom@yahoo.com>
2025-07-22 19:25:40 +0200

    Minor changes (again)

--- dataset_maker/download_dataset.py
+++ dataset_maker/download_dataset.py
@@ -1,17 +1,56 @@
-from huggingface_hub import snapshot_download
+# from huggingface_hub import snapshot_download
+#
+# def download_full_dataset():
+#     folder = snapshot_download(
+#                     "HuggingFaceFW/fineweb-2",
+#                     repo_type="dataset",
+#                     local_dir="./data/fineweb2/",
+#                     # download the Ita filtered + test data
+#                     allow_patterns=["data/ita_Latn/train/*", "data/ita_Latn/test/*"],)
+#     return folder
+#
+# TRAIN_SPLIT, TEST_SPLIT = 'train', 'test'
+# def get_dataset_stream(split:str):
+#     from datasets import load_dataset
+#     # get Croatian data
+#     fw = load_dataset("HuggingFaceFW/fineweb-2", name="ita_Latn", split=split, streaming=True)
+#     return fw
 
-def download_full_dataset():
-    folder = snapshot_download(
-                    "HuggingFaceFW/fineweb-2",
-                    repo_type="dataset",
-                    local_dir="./data/fineweb2/",
-                    # download the Ita filtered + test data
-                    allow_patterns=["data/ita_Latn/train/*", "data/ita_Latn/test/*"],)
-    return folder
+import os.path
+from datasets import load_dataset
+from tqdm import tqdm
 
-TRAIN_SPLIT, TEST_SPLIT = 'train', 'test'
-def get_dataset_stream(split:str):
-    from datasets import load_dataset
-    # get Croatian data
-    fw = load_dataset("HuggingFaceFW/fineweb-2", name="ita_Latn", split=split, streaming=True)
-    return fw
+from dataset_maker.job_server import Database
+mc4_it_full_stream = load_dataset("gsarti/clean_mc4_it", "tiny", streaming=False, cache_dir='.cache')
+
+train = mc4_it_full_stream['train']
+validation = mc4_it_full_stream['validation']
+
+base_uri = os.path.join('.', 'data', 'clean_mc4_it', 'tiny')
+sqlite_db = Database()
+# sqlite_db.create_database()
+# quit()
+batch_phrases = []
+for i, entry in tqdm(enumerate(train)):
+    batch_phrases.append((i, entry['text'], 0, 1))
+    if (i % 1_000) == 0:
+        sqlite_db.add_phrase(batch_phrases, None, None)
+        sqlite_db.conn.commit()
+        batch_phrases = []
+
+if len(batch_phrases) > 0:
+    sqlite_db.add_phrase(batch_phrases, None, None)
+sqlite_db.conn.commit()
+
+batch_phrases.clear()
+
+for i, entry in tqdm(enumerate(validation)):
+    batch_phrases.append((i, entry['text'], 0, 0))
+    if (i % 1_000) == 0:
+        sqlite_db.add_phrase(batch_phrases, None, None)
+        sqlite_db.conn.commit()
+        batch_phrases = []
+
+if len(batch_phrases) > 0:
+    sqlite_db.add_phrase(batch_phrases, None, None)
+sqlite_db.conn.commit()
--- dataset_maker/job_server.py
+++ dataset_maker/job_server.py
@@ -2,18 +2,15 @@
 This module simply serve multiple processes of the next text to translate.
 It simply keeps track of the next sentence.
 """
-import mysql.connector
-from mysql.connector import IntegrityError
+import sqlite3
+from sqlite3 import IntegrityError
+
 from tqdm import tqdm
 
 from dataset_maker.dataset import load_clean_mc4_dataset
 
-hostName = "localhost"
-serverPort = 8080
-dataset_path = './data/clean_mc4_it/clean_mc4_it.jsonl'
-sorianese_dataset_path = './data/fineweb-2/data/sor_Latn'
 
-NOT_DONE = -1
+NOT_DONE = 0
 WORK_IN_PROGRESS = 0
 DONE = 1
 
@@ -25,16 +22,8 @@ class Database:
 
 
     def open_connection(self):
-        return mysql.connector.connect(
-            # host="er-sorianese",
-            host="127.0.0.1",
-            port="3306",
-            user="root",
-            password="root",
-            database = "sentence_db",
-            charset='utf8mb4',  # <-- important
-            use_unicode=True,
-            autocommit=False,
+        return sqlite3.connect(
+            "er-sorianese",
         )
 
     def get_cursor(self):
@@ -46,66 +35,43 @@ class Database:
         eseguendo i comandi SQL necessari.
         """
         schema_sqls = [
-            """CREATE TABLE IF NOT EXISTS BatchJob (
-                batch_id VARCHAR(47) PRIMARY KEY,
-                status INT DEFAULT 0
-            );""",
+            # """CREATE TABLE IF NOT EXISTS BatchJob (
+            #     batch_id VARCHAR(47) PRIMARY KEY,
+            #     status INT DEFAULT 0
+            # );""",
             """CREATE TABLE IF NOT EXISTS ItaSentence (
                 sentence_id BIGINT PRIMARY KEY,
                 sentence_text MEDIUMTEXT,
-                status INT DEFAULT -1,
-                time_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                batch_id VARCHAR(47)
-            ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;""",
+                status INT DEFAULT 0,
+                train INT DEFAULT 1
+            );""",
 
             """CREATE TABLE IF NOT EXISTS SorSentence (
                 sentence_id BIGINT PRIMARY KEY,
                 sentence_text MEDIUMTEXT
-            ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"""
+            )"""
         ]
 
-        job_sql = f"""
-            CREATE EVENT IF NOT EXISTS restore_status
-            ON SCHEDULE EVERY 1 MINUTE
-            STARTS '2025-04-04 19:19:37'
-            ENABLE
-            DO
-            UPDATE ItaSentence
-            SET status = {NOT_DONE}
-            WHERE status = {WORK_IN_PROGRESS} AND time_created < DATE_SUB(NOW(), INTERVAL 1 MINUTE);
-        """
-        trigger_sql = """
-                CREATE TRIGGER IF NOT EXISTS update_items_trigger
-                BEFORE UPDATE ON ItaSentence
-                FOR EACH ROW
-                UPDATE ItaSentence SET time_created=CURRENT_TIMESTAMP()
-                WHERE NEW.sentence_id=sentence_id;"""
         cursor = self.get_cursor()
         try:
             for stmt in schema_sqls:
                 cursor.execute(stmt)
-            # cursor.execute(job_sql)
-            # cursor.execute(trigger_sql)
             self.conn.commit()
         finally:
             cursor.close()
             self.conn.close()
 
         self.conn = self.open_connection()
-        # Esegui le istruzioni SQL (può contenerne più di una)
         return self.conn
 
-    def populate_database(self, phrases: list[dict]):
-        self.conn = self.open_connection()
+    def add_phrase(self, phrase: list[str], sentence_id: list[int], train: list[int]):
+        # self.conn = self.open_connection()
         cursor = self.get_cursor()
-
         try:
-            for p in tqdm(phrases):
-                sid, stext = p['id'], p['text']
-                cursor.execute(f"INSERT INTO ItaSentence (sentence_id, sentence_text, status) VALUES (%s, %s, {NOT_DONE})", (sid, stext))
+            cursor.executemany(f"INSERT INTO ItaSentence (sentence_id, sentence_text, status, train) VALUES (?, ?, ?, ?);",phrase)
         except IntegrityError as e:
             pass
-        self.conn.commit()
+        # self.conn.commit()
 
     def get_next_item(self):
         cursor = self.get_cursor()
@@ -119,6 +85,24 @@ class Database:
 
             cursor.execute("UPDATE ItaSentence SET status=%s, time_created=CURRENT_TIMESTAMP() WHERE sentence_id=%s", (WORK_IN_PROGRESS, results[0]) )
             self.conn.commit()
+
+            return results
+
+        except Exception as e:
+            print("Error:", e)
+            self.conn.rollback()  # rollback on error
+        finally:
+            cursor.close()
+
+    def get_next_batch_items(self, batch_size: int):
+        cursor = self.get_cursor()
+        try:
+            cursor.execute(f"SELECT sentence_id, sentence_text FROM ItaSentence WHERE status={NOT_DONE} LIMIT ? FOR UPDATE", (batch_size,))
+            results = cursor.fetchall()
+
+            if len(results) == 0:
+                return None
+            results = results.pop()
 
             return results
 
@@ -151,50 +135,17 @@ class Database:
         finally:
             cursor.close()
 
-    def add_batch_job(self, batch_id, custom_ids):
+
+    def update_batch_job(self, sentences, status):
         cursor = self.get_cursor()
         try:
-            cursor.execute("INSERT INTO BatchJob (batch_id) VALUES (%s)", (batch_id,))
-            for cid in custom_ids:
-                cursor.execute("UPDATE ItaSentence SET batch_id = %s WHERE sentence_id = %s", (batch_id, cid))
+            cursor.execute("Update ItaSentence SET status = %s WHERE sentence_id = %s", (status, sentences))
             self.conn.commit()
         except Exception as e:
             print("Error inserting new translation: ", e)
         finally:
             cursor.close()
 
-    def update_batch_job(self, batch_id, status):
-        cursor = self.get_cursor()
-        try:
-            cursor.execute("Update BatchJob SET status = %s WHERE batch_id = %s", (status, batch_id))
-            cursor.execute("Update ItaSentence SET status = %s WHERE batch_id = %s", (status, batch_id))
-            self.conn.commit()
-        except Exception as e:
-            print("Error inserting new translation: ", e)
-        finally:
-            cursor.close()
-
-    def get_pending_batch_jobs(self):
-        cursor = self.get_cursor()
-        try:
-            cursor.execute("SELECT batch_id FROM BatchJob WHERE status = %s", (WORK_IN_PROGRESS,))
-            results = cursor.fetchall()
-            return [r[0] for r in results]
-        except Exception as e:
-            print("Error inserting new translation: ", e)
-        finally:
-            cursor.close()
-
-    def has_pending_jobs(self) -> int:
-        cursor = self.get_cursor()
-        try:
-            cursor.execute("SELECT COUNT(batch_id) FROM BatchJob WHERE status = %s", (WORK_IN_PROGRESS,))
-            results = cursor.fetchall()
-            return results.pop()[0]
-        except Exception as e:
-            print("Error inserting new translation: ", e)
-        finally:
-            cursor.close()
 
     def completion_status(self):
         query = """SELECT
